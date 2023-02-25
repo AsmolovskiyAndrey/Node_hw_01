7b40510a602016cc3a87f42df38864faab1f1bb0
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2023-02-25 14:37:18 +0100

    finisch

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Node_hw_01
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+node index.js --action list
+https://drive.google.com/file/d/1-nG5Y2fmO5qzWaHQGR7zDRt5T9TRauhz/view?usp=share_link
+=====================================================================================
+
+# Получаем контакт по id
+
+node index.js --action get --id 5
+https://drive.google.com/file/d/10-GAItTRxfHyCE7dbtK2AwUWrNSXflGh/view?usp=sharing
+=====================================================================================
+
+# Добавялем контакт
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+https://drive.google.com/file/d/100RyyrKubtLkcBnrcMDb5eypiFs5EYx_/view?usp=sharing
+=====================================================================================
+
+# Удаляем контакт
+
+node index.js --action remove --id=3
+https://drive.google.com/file/d/1044BE22MuKi93wfykYv-g2CgS5DVbpG3/view?usp=sharing
--- contacts.js
+++ contacts.js
@@ -1,31 +1,25 @@
-// const path = require("path");
 const path = require("path");
 const fs = require("fs").promises;
 const uniqid = require("uniqid");
 
 const contactsPath = path.resolve("contacts.json");
 
-console.log(contactsPath);
-
-// TODO: задокументировать каждую функцию
 async function listContacts() {
   const list = await fs.readFile("db/contacts.json");
-  // console.log(JSON.parse(list));
   return JSON.parse(list);
 }
 
 async function getContactById(contactId) {
   const data = await listContacts();
   const contactById = data.find(({ id }) => id === contactId);
-  // console.log(contactById);
+  return contactById;
 }
-// getContactById("3");
 
 async function removeContact(contactId) {
   const data = await listContacts();
   const deleteContactById = data.filter(({ id }) => !(id === contactId));
-  // console.log(deleteContactById);
   fs.writeFile("db/contacts.json", JSON.stringify(deleteContactById));
+  console.log(`Contact with id ${contactId} was deleted successfully`);
 }
 
 async function addContact(name, email, phone) {
@@ -38,7 +32,7 @@ async function addContact(name, email, phone) {
   const data = await listContacts();
   const newData = [...data, newContact];
   await fs.writeFile("db/contacts.json", JSON.stringify(newData));
-  console.log(data);
+  console.log("Contact added successfully ");
 }
 
-addContact("hjh", "asd", 456);
+module.exports = { listContacts, addContact, removeContact, getContactById };
